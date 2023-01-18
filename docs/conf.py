fa5aff39f66d81414ce3014944889ff76d10512c
Srijith Balakrishnan <inform.srijith@gmail.com>
2023-01-18 18:45:19 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-import dreaminsg_integrated_model
+sys.path.insert(0, os.path.abspath(".."))
+
+import infrarisk
 
 # -- General configuration ---------------------------------------------
 
@@ -31,22 +32,22 @@ import dreaminsg_integrated_model
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'dreaminsg-integrated-model'
+project = "dreaminsg-integrated-model"
 copyright = "2021, Srijith Balakrishnan"
 author = "Srijith Balakrishnan"
 
@@ -69,10 +70,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -83,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -94,13 +95,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dreaminsg_integrated_modeldoc'
+htmlhelp_basename = "dreaminsg_integrated_modeldoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -109,15 +110,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -127,9 +125,13 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'dreaminsg_integrated_model.tex',
-     'dreaminsg-integrated-model Documentation',
-     'Srijith Balakrishnan', 'manual'),
+    (
+        master_doc,
+        "dreaminsg_integrated_model.tex",
+        "dreaminsg-integrated-model Documentation",
+        "Srijith Balakrishnan",
+        "manual",
+    ),
 ]
 
 
@@ -138,9 +140,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'dreaminsg_integrated_model',
-     'dreaminsg-integrated-model Documentation',
-     [author], 1)
+    (
+        master_doc,
+        "dreaminsg_integrated_model",
+        "dreaminsg-integrated-model Documentation",
+        [author],
+        1,
+    )
 ]
 
 
@@ -150,13 +156,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'dreaminsg_integrated_model',
-     'dreaminsg-integrated-model Documentation',
-     author,
-     'dreaminsg_integrated_model',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "dreaminsg_integrated_model",
+        "dreaminsg-integrated-model Documentation",
+        author,
+        "dreaminsg_integrated_model",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
-
-
-
