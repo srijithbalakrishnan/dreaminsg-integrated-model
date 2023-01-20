4c85c81dab7f06efd830645e7a324e8e0c2a9b21
Srijith Balakrishnan <inform.srijith@gmail.com>
2023-01-20 13:52:51 +0800

    Update resilience_metrics.py

--- infrarisk/src/resilience_metrics.py
+++ infrarisk/src/resilience_metrics.py
@@ -347,9 +347,9 @@ class WeightedResilienceMetric:
         """Calculates the area under a curve
 
         :param x: An array of the x values of the curve
-        :type x: array of floats
+        :type x: list
         :param y: An array of the y values of the curve
-        :type y:array of floats
+        :type y: list
         :return: The area under the curve
         :rtype: float
         """
