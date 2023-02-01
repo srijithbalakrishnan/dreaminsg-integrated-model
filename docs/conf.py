94e35bd2b7c14ad281fa7abd909094da1cc204e3
Srijith Balakrishnan <inform.srijith@gmail.com>
2023-02-01 20:53:32 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,9 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 html_logo = "logo.PNG"
-
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 
 numfig = True
